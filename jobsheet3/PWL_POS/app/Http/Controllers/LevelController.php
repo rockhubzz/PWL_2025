c34f7871105fbced12dd3949035944049c69902a
Rocky Alessandro <compatiblewindows7@gmail.com>
2025-03-06 07:58:58 +0700

    jobsheet3_praktikum6

--- jobsheet3/PWL_POS/app/Http/Controllers/LevelController.php
+++ jobsheet3/PWL_POS/app/Http/Controllers/LevelController.php
@@ -8,9 +8,9 @@ use Illuminate\Support\Facades\DB;
 class LevelController extends Controller
 {
     public function index(){
-        // DB::insert('insert into m_level(level_kode, level_nama)
-        //             values(?, ?)', ['CUS', 'Pelanggan']);
-        // return 'Insert data baru berhasil';
+        DB::insert('insert into m_level(level_kode, level_nama)
+                    values(?, ?)', ['CUS', 'Pelanggan']);
+        return 'Insert data baru berhasil';
 
         // $row = DB::update('update m_level set level_nama = ?
         //                     where level_kode = ?', ['Customer', 'CUS']);
