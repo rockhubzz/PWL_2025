5a45f2ef86abdcd9e33d989cafbef6d5d9110ba4
Rocky Alessandro <compatiblewindows7@gmail.com>
2025-03-12 12:56:58 +0700

    jobsheet4_praktikum2.4

--- jobsheet4/PWL_POS/app/Models/UserModel.php
+++ jobsheet4/PWL_POS/app/Models/UserModel.php
@@ -13,5 +13,5 @@ class UserModel extends Model
     protected $primaryKey = 'user_id';
     public $timestamps = false;
     // protected $fillable = ['level_id', 'username', 'nama', 'password'];
-    protected $fillable = ['level_id', 'username', 'nama'];
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
 }
