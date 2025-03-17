9a0df9c6bef7126213e526bb9bc40d1a7c9d3091
Rocky Alessandro <compatiblewindows7@gmail.com>
2025-03-17 22:49:43 +0700

    jobsheet5_praktikum2

--- jobsheet5/PWL_POS/app/Http/Controllers/KategoriController.php
+++ jobsheet5/PWL_POS/app/Http/Controllers/KategoriController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\DataTables\KategoriDataTable;
 
 class KategoriController extends Controller
 {
-    public function index(){
+    public function index(KategoriDataTable $dataTable){
         // $data = [
         //     'kategori_kode' => 'SNK',
         //     'kategori_nama' => 'Snack/Makanan Ringan'
@@ -23,7 +24,8 @@ class KategoriController extends Controller
         //         ->where('kategori_kode', 'SNK')->delete();
         // return 'Delete data berhasil. Jumlah data yang dihapus: '.$row.' baris';
     
-        $data = DB::table('m_kategori')->get();
-        return view('kategori', ['data'=>$data]);
+        // $data = DB::table('m_kategori')->get();
+        // return view('kategori', ['data'=>$data]);
+        return $dataTable->render('kategori.index');
     }
 }
---
+++ jobsheet5/PWL_POS/resources/views/kategori/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.app')
+
+@section('content')
+    <div class="container">
+        <div class="card">
+            <div class="card-header">Manage Kategori</div>
+            <div class="card-body">
+                {{$dataTable->table()}}
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('scripts')
+    {{$dataTable->scripts()}}
+@endpush
