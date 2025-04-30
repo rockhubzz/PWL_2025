e109d6fa3cd6588dbd9372ef11a6f05712953fd0
Rocky Alessandro <compatiblewindows7@gmail.com>
2025-04-30 19:27:25 +0700

    jobsheet10_praktikum4

--- jobsheet10/routes/api.php
+++ jobsheet10/routes/api.php
@@ -24,3 +24,11 @@ Route::middleware('auth:api')->get('/user', function(Request $request){
     return $request->user();
 });
 Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+
+use App\Http\Controllers\Api\LevelController;
+
+Route::get('levels', [LevelController::class, 'index']);
+Route::post('levels', [LevelController::class, 'store']);
+Route::get('levels/{level}', [LevelController::class, 'show']);
+Route::put('levels/{level}', [LevelController::class, 'update']);
+Route::delete('levels/{level}', [LevelController::class, 'destroy']);
