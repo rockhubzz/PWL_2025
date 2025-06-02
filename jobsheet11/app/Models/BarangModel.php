43d1f97656077d3060faf53c62e55202e0448f81
Rocky Alessandro <compatiblewindows7@gmail.com>
2025-06-02 23:20:08 +0700

    jobsheet11_tugas

--- jobsheet11/app/Models/BarangModel.php
+++ jobsheet11/app/Models/BarangModel.php
@@ -11,10 +11,16 @@ class BarangModel extends Model{
     protected $table = 'm_barang';
     protected $primaryKey = 'barang_id';
 
-    protected $fillable = ['kategori_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual'];
+    protected $fillable = ['kategori_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual', 'image'];
 
     public function kategori(): BelongsTo{
         return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
+    }
+
+    protected function image(){
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts/' . $image)
+        );
     }
 
 }
